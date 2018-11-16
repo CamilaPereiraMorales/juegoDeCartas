d290670a0fe91ac48b09bae9640eee99e77b5c5d
Camili <camipereira2111@gmail.com>
2018-11-15 22:04:06 -0300

    carpeta

---
+++ memory/build/precache-manifest.60df8ecef3c66df1bd3cc2e854834662.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a66b76c15e9f273d1868f3cfb016362",
+    "url": "/juegoDeCartas/static/media/fondo.1a66b76c.jpeg"
+  },
+  {
+    "revision": "e2c0457ac181639b2698",
+    "url": "/juegoDeCartas/static/css/main.5190414c.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/juegoDeCartas/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "33079dba16c3fbed8a1f",
+    "url": "/juegoDeCartas/static/js/1.33079dba.chunk.js"
+  },
+  {
+    "revision": "c87dd1e60f3285278895",
+    "url": "/juegoDeCartas/static/js/runtime~main.c87dd1e6.js"
+  },
+  {
+    "revision": "1154cee8d83389cee0b218b6cb0b7c57",
+    "url": "/juegoDeCartas/static/media/baraja.1154cee8.jpg"
+  },
+  {
+    "revision": "e2c0457ac181639b2698",
+    "url": "/juegoDeCartas/static/js/main.e2c0457a.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/juegoDeCartas/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/juegoDeCartas/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/juegoDeCartas/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/juegoDeCartas/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "33079dba16c3fbed8a1f",
+    "url": "/juegoDeCartas/static/css/1.9a71a382.chunk.css"
+  },
+  {
+    "revision": "d2d066a5af598cef7aa1338c43c1625a",
+    "url": "/juegoDeCartas/index.html"
+  }
+];
